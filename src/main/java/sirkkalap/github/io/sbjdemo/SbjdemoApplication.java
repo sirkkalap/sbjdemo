3b0a1585c3670a47203437b26417196070e41f11
Petri Sirkkala <petri.sirkkala@solita.fi>
2025-06-16 13:19:27 +0300

    Initial from https://start.spring.io/ with https://spring.io/quickstart

---
+++ src/main/java/sirkkalap/github/io/sbjdemo/SbjdemoApplication.java
@@ -0,0 +1,13 @@
+package sirkkalap.github.io.sbjdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbjdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbjdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sirkkalap/github/io/sbjdemo/SbjdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sirkkalap.github.io.sbjdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbjdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
